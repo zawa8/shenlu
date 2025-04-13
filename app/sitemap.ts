47e523687c47bc171ec1c071274d5b079e6c25fa
viml kumar <vimalkg@gmail.com>
2025-04-13 13:53:23 +0530

    zawa8.vercel.app mck82.vercel.app fb.com/ztrabc x.com/etphor

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,6 +3,11 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const blogs = allPosts.map((post) => ({ url: `https://aaztk.vercel.app/blog/${post.slug}`, lastModified: post.publishedAt, }));
-  const routes = ["", "/blog", "/about"].map((route) => ({ url: `https://aaztk.vercel.app${route}`, lastModified: new Date().toISOString().split("T")[0], }));
+  const routes = ["", "/blog", "/about"].map((route) => (
+		{
+			url: `https://aaztk.vercel.app${route}`,
+			lastModified: new Date().toISOString().split("T")[0],
+		}
+  ));
   return [...routes, ...blogs];
 }
